e5218599de11da5b9d128b12826357ce62ee5c7c
Sushant Kumar <sks20012003@gmail.com>
2025-02-10 01:36:04 +0530

    app version 1 is ready

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,17 @@ import messageRoutes from './routes/message.route.js';
 import {app, server} from './socket/socket.js';
 import cookieParser from "cookie-parser";
 import './services/cronJobs.js';
+import path from 'path';
 
 dotenv.config({});
 
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'http://localhost:8000',
     credentials:true
 }));
 app.use(cookieParser());
@@ -24,6 +26,13 @@ app.use(cookieParser());
 //routing 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+
+//serving static files
+app.use(express.static(path.join(__dirname, 'frontend' , 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+})
 
 
 server.listen(PORT, ()=>{
